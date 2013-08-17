24a231bd827bd74a6a665c66cf4ea2a5d97feeb3
Drew Conway <agconway@gmail.com>
2013-08-17 12:25:09 -0400

    Output vocab table as Markdown in README.md file

--- vocab.R
+++ vocab.R
@@ -7,3 +7,17 @@ HTML(vocab,
 	 decimal.mark = ",",
 	 row.names = FALSE)
 # Post-process to escape ampersands, remove HTML cruft and tidy HTML.
+
+# Add table to the README.md so things render directly to Github page
+library(ascii)
+
+# Generate Markdown for table
+vocab.md <- capture.output(print(ascii(vocab[2:nrow(vocab),], include.rownames=F), 
+                                 type="pandoc"))
+vocab.md <- paste(vocab.md, collapse="\n")
+
+readme.file <- file('README.md', open="w")
+writeLines(vocab.md, readme.file)
+close(readme.file)
+
+
